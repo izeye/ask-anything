3fe853e6efaa689f0c16c748986605db1f436160
Johnny Lim <izeye@naver.com>
2024-01-13 16:22:04 +0900

    Update to align with the changes from Spring AI
    See https://github.com/spring-projects/spring-ai/commit/98281ad1ef0b014b723a87de47661abc84cd7793

--- src/main/java/com/izeye/app/askanything/config/AiConfig.java
+++ src/main/java/com/izeye/app/askanything/config/AiConfig.java
@@ -1,9 +1,8 @@
 package com.izeye.app.askanything.config;
 
 import com.izeye.app.askanything.domain.AiModels;
-import org.springframework.ai.client.AiClient;
+import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.ai.openai.api.OpenAiApi;
-import org.springframework.ai.openai.client.OpenAiClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,24 +15,24 @@ import org.springframework.context.annotation.Configuration;
 public class AiConfig {
 
 	@Bean
-	public AiClient openAiClientWithGpt35(OpenAiApi openAiApi) {
-		OpenAiClient openAiClient = new OpenAiClient(openAiApi);
-		openAiClient.setModel(AiModels.GPT_3_5.getId());
-		return openAiClient;
+	public OpenAiChatClient openAiChatClientWithGpt35(OpenAiApi openAiApi) {
+		OpenAiChatClient openAiChatClient = new OpenAiChatClient(openAiApi);
+		openAiChatClient.setModel(AiModels.GPT_3_5.getId());
+		return openAiChatClient;
 	}
 
 	@Bean
-	public AiClient openAiClientWithGpt4(OpenAiApi openAiApi) {
-		OpenAiClient openAiClient = new OpenAiClient(openAiApi);
-		openAiClient.setModel(AiModels.GPT_4.getId());
-		return openAiClient;
+	public OpenAiChatClient openAiChatClientWithGpt4(OpenAiApi openAiApi) {
+		OpenAiChatClient openAiChatClient = new OpenAiChatClient(openAiApi);
+		openAiChatClient.setModel(AiModels.GPT_4.getId());
+		return openAiChatClient;
 	}
 
 	@Bean
-	public AiClient openAiClientWithGpt4Turbo(OpenAiApi openAiApi) {
-		OpenAiClient openAiClient = new OpenAiClient(openAiApi);
-		openAiClient.setModel(AiModels.GPT_4_TURBO.getId());
-		return openAiClient;
+	public OpenAiChatClient openAiChatClientWithGpt4Turbo(OpenAiApi openAiApi) {
+		OpenAiChatClient openAiChatClient = new OpenAiChatClient(openAiApi);
+		openAiChatClient.setModel(AiModels.GPT_4_TURBO.getId());
+		return openAiChatClient;
 	}
 
 }
--- src/main/java/com/izeye/app/askanything/service/DefaultAnswerService.java
+++ src/main/java/com/izeye/app/askanything/service/DefaultAnswerService.java
@@ -4,7 +4,7 @@ import com.izeye.app.askanything.domain.AiModels;
 import com.izeye.app.askanything.domain.Answer;
 import com.izeye.app.askanything.domain.Question;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.client.AiClient;
+import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,27 +18,27 @@ import java.util.List;
 @Slf4j
 public class DefaultAnswerService implements AnswerService {
 
-	private final AiClient openAiClientWithGpt35;
+	private final OpenAiChatClient openAiChatClientWithGpt35;
 
-	private final AiClient openAiClientWithGpt4;
+	private final OpenAiChatClient openAiChatClientWithGpt4;
 
-	private final AiClient openAiClientWithGpt4Turbo;
+	private final OpenAiChatClient openAiChatClientWithGpt4Turbo;
 
-	public DefaultAnswerService(AiClient openAiClientWithGpt35, AiClient openAiClientWithGpt4,
-			AiClient openAiClientWithGpt4Turbo) {
-		this.openAiClientWithGpt35 = openAiClientWithGpt35;
-		this.openAiClientWithGpt4 = openAiClientWithGpt4;
-		this.openAiClientWithGpt4Turbo = openAiClientWithGpt4Turbo;
+	public DefaultAnswerService(OpenAiChatClient openAiChatClientWithGpt35, OpenAiChatClient openAiChatClientWithGpt4,
+			OpenAiChatClient openAiChatClientWithGpt4Turbo) {
+		this.openAiChatClientWithGpt35 = openAiChatClientWithGpt35;
+		this.openAiChatClientWithGpt4 = openAiChatClientWithGpt4;
+		this.openAiChatClientWithGpt4Turbo = openAiChatClientWithGpt4Turbo;
 	}
 
 	@Override
 	public List<Answer> ask(Question question) {
 		log.info("Question: {}", question);
-		String answerTextFromGpt35 = this.openAiClientWithGpt35.generate(question.text());
+		String answerTextFromGpt35 = this.openAiChatClientWithGpt35.generate(question.text());
 		Answer answerFromGpt35 = new Answer(answerTextFromGpt35, AiModels.GPT_3_5.getId());
-		String answerTextFromGpt4 = this.openAiClientWithGpt4.generate(question.text());
+		String answerTextFromGpt4 = this.openAiChatClientWithGpt4.generate(question.text());
 		Answer answerFromGpt4 = new Answer(answerTextFromGpt4, AiModels.GPT_4.getId());
-		String answerTextFromGpt4Turbo = this.openAiClientWithGpt4Turbo.generate(question.text());
+		String answerTextFromGpt4Turbo = this.openAiChatClientWithGpt4Turbo.generate(question.text());
 		Answer answerFromGpt4Turbo = new Answer(answerTextFromGpt4Turbo, AiModels.GPT_4_TURBO.getId());
 		List<Answer> answers = List.of(answerFromGpt35, answerFromGpt4, answerFromGpt4Turbo);
 		log.info("Answers: {}", answers);
